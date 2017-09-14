791d8ef131bcbcf7a6eeb05ad001ecac170c348a
N8 <nate.handy@gmail.com>
2017-09-14 12:10:32 -0600

    no more globalswq

--- src/states/play.js
+++ src/states/play.js
@@ -10,13 +10,6 @@ require('../../assets/images/bomb.png');
 require('../../assets/images/arrow.png');
 require('../../assets/images/brake.png');
 require('../../assets/images/blueGem.png');
-import dropRates from '../../assets/json/drop_rates.json';
-var bg_cave;
-var cart;
-var tracks;
-var full_speed = 0;
-var fix_position = 0;
-var booster = 0;
 
 export default class PlayState extends Phaser.State {
     preload () {
@@ -28,16 +21,17 @@ export default class PlayState extends Phaser.State {
         this.game.load.image('arrow', '/assets/arrow.png');
         this.game.load.image('brake', '/assets/brake.png');
         this.game.load.image('blueGem', '/assets/blueGem.png');
+        this.booster = 0;
     }
 
     create () {
         this.game.keyboard = new Phaser.Keyboard(this.game);
         this.game.dpad = this.game.keyboard.createCursorKeys();
         this.bgCave = this.game.add.tileSprite(0, 0, 800, 600, 'this.bgCave');
-        tracks = this.game.add.group();
-        tracks.enableBody = true;
+        this.tracks = this.game.add.group();
+        this.tracks.enableBody = true;
         for (var i = 0, len = 100; i < len; i += 35) {
-            this.t = tracks.create(i, this.game.height * 0.8, 'track_str');
+            this.t = this.tracks.create(i, this.game.height * 0.8, 'track_str');
             this.t.body.immovable = true;
         }
         this.cartType = this.game.add.group();
@@ -49,8 +43,8 @@ export default class PlayState extends Phaser.State {
         this.cart.body.checkCollision.up = true;
         this.cart.body.checkCollision.down = true;
         this.game.backButton = this.game.add.button(0, 0, 'back_button', function () { this.game.state.start('MainMenu') }, this);
-        this.game.boostButton = this.game.add.button(this.game.width - 100, 0, 'arrow', function(){booster += 50}, this);
-        this.game.brakeButton = this.game.add.button(this.game.width - 200, 0, 'brake', function(){booster -= 50}, this);
+        this.game.boostButton = this.game.add.button(this.game.width - 100, 0, 'arrow', function(){this.booster += 50}, this);
+        this.game.brakeButton = this.game.add.button(this.game.width - 200, 0, 'brake', function(){this.booster -= 50}, this);
         this.gemSpawner = new Spawner(this.game, this.game.width + 15, 200, this.game.width + 15, 600, ['gem', 1]);
         this.bombSpawner = new Spawner(this.game, this.game.width + 15, 200, this.game.width + 15, 600, [Bomb]);
     }
@@ -63,20 +57,20 @@ export default class PlayState extends Phaser.State {
             this.spawn();
         }
 
-        var firstAlive = tracks.getFirstAlive();
+        var firstAlive = this.tracks.getFirstAlive();
         if (firstAlive.x < -35) { // Cleanup as we go
-            tracks.removeChild(firstAlive);
+            this.tracks.removeChild(firstAlive);
         }
         // Make sure collision with tracks happens correctly, may need to do a callback to make sure it goes up hill?
-        this.game.physics.arcade.collide(this.cartType, tracks, null, null, null);
-        this.syncCart(this.cart, tracks);
+        this.game.physics.arcade.collide(this.cartType, this.tracks, null, null, null);
+        this.syncCart(this.cart, this.tracks);
     }
 
     spawn () {
         var currentVelocity = this.t.body.velocity.x
         var currentX = this.t.body.x
 
-        this.t = tracks.create(currentX + 35, this.game.height * 0.8, 'track_str');
+        this.t = this.tracks.create(currentX + 35, this.game.height * 0.8, 'track_str');
         this.t.body.velocity.x = currentVelocity;
         this.t.body.immovable = true;
 
@@ -98,19 +92,19 @@ export default class PlayState extends Phaser.State {
         var modifier = 0;
         if (myCart.body.x > 150) {
             modifier = 20;
-            fix_position = 1;
+            this.fix_position = 1;
         } else if (myCart.body.x < 20) {
             modifier = -20;
-            fix_position = 1;
-        } else if (fix_position && (myCart.body.x >= 70 && myCart.body.x <= 80)) {
+            this.fix_position = 1;
+        } else if (this.fix_position && (myCart.body.x >= 70 && myCart.body.x <= 80)) {
             // Move the cart until it gets up to full speed
             this.setTrackSpeed(trackGroup, -(myCart.body.velocity.x));
-            fix_position = 0;
+            this.fix_position = 0;
         } else {
-            if (myCart.body.velocity.x < (200 + booster)) {
+            if (myCart.body.velocity.x < (200 + this.booster)) {
                 myCart.body.velocity.x += 20;
             // Move the cart until it gets back close to full speed
-            } else if (myCart.body.velocity.x > (200 + booster + 20)) {
+            } else if (myCart.body.velocity.x > 100 && myCart.body.velocity.x > (200 + this.booster + 20)) {
                 myCart.body.velocity.x -= 20;
             }
         }
