59d737188e17af48b366c0ad8687441d823437f0
DizzyDogg <jeffthetenor@yahoo.com>
2017-09-14 14:51:01 -0600

    backwards portal logic

--- src/objects/items/portal.js
+++ src/objects/items/portal.js
@@ -3,7 +3,7 @@ import Item from '../item';
 export default class Portal extends Item {
     constructor (game, x, y) {
         super(game, x, y, 'portal');
-        if (this.player.getInfo()['gems'] >= 5) {
+        if (this.player.getInfo()['gems'] < 5) {
             this.spawnRate = 0.00;
         }
         else {
