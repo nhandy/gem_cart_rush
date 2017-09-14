2e3f71467f39c279ef91caab42aa22cfaaa74d87
Kaycie Goodman <jgoodman@bluehost.com>
2017-09-14 10:49:24 -0600

    Adding armor and shield images into shop

--- src/states/shop.js
+++ src/states/shop.js
@@ -1,12 +1,21 @@
+for (var i in shopconfig.items) {
+	var item = shopconfig.items[i];
+	require('../../assets/images/' + item.file);
+}
 import shopconfig from '../../assets/json/shop_config.json';
 export default class ShopState extends Phaser.State {
     preload() {
         this.game.load.image('back_button', '/assets/Back.png');
+		for (var i in shopconfig.items) {
+			var item = shopconfig.items[i];
+			this.game.load.image(item.name, 'assets/' + item.file);
+		}
     }
 
     create() {
         this.game.stage.backgroundColor = '#182d3b';
         this.game.shopButton = this.game.add.button(0, 0, 'back_button', function(){this.game.state.start('MainMenu')}, this);
+
     }
 
     update() {
