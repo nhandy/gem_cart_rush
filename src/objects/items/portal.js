1586772257e21d62712812ab10f9c1eecf06f568
DizzyDogg <jeffthetenor@yahoo.com>
2017-09-14 14:39:54 -0600

    conflicts

--- src/objects/items/portal.js
+++ src/objects/items/portal.js
@@ -3,10 +3,12 @@ import Item from '../item';
 export default class Portal extends Item {
     constructor (game, x, y) {
         super(game, x, y, 'portal');
-        // if this.player.getInfo()['gems'] >= 100
-        // this.spawnRate = 0.01;
-        // else
-        this.spawnRate = 0.005;
+        if (this.player.getInfo()['gems'] >= 5) {
+            this.spawnRate = 0.01;
+        }
+        else {
+            this.spawnRate = 0.01;
+        }
 
         this.scale.setTo(0.30);
     }
